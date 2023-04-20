dfd436e1d05c031c8a72f3db3e185c298f84912b
caiosald <caiors1@al.insper.edu.br>
2023-04-20 14:01:38 -0300

    NAVE SE MOVE - FALTA ADD O DELTA T

--- funcoes.py
+++ funcoes.py
@@ -9,8 +9,8 @@ class Jogo:
         pygame.display.set_caption('Jogo do Caião e do Benizão')
         imagem_nave = pygame.image.load('nave do joguinho.png')
         imagem_nave_redimensionada = pygame.transform.scale(imagem_nave, (100,100))
-        self.nave = Nave(self.window, 380, 670, imagem_nave_redimensionada, 500, 500)
-        self.nave2 = Nave(self.window, 1140, 670, imagem_nave_redimensionada, 500, 500)
+        self.nave = Nave(self.window, [380, 670], imagem_nave_redimensionada, 0)
+        self.nave2 = Nave(self.window, [1140, 670], imagem_nave_redimensionada, 0)
 
     def atualiza_estado(self):
         #Caso o jogador clique no "quit" o jogo fecha:
@@ -18,8 +18,9 @@ class Jogo:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 jogo = False
-        self.nave.movimenta_nave()
-        self.nave2.movimenta_nave()
+            else:
+                self.nave.movimenta_nave(evento)
+        # self.nave2.movimenta_nave()
 
         return jogo
         
@@ -32,38 +33,39 @@ class Jogo:
         pygame.display.update()
     
     def loop_jogo(self):
-
+        self.atualiza_estado()
         while self.atualiza_estado():
             self.desenha()
 
 class Nave:
     
-    def __init__(self,window,posx,posy,imagem_redimensionada,vel_navex, vel_navey):
-        self.posicao_nave_x = posx
-        self.posicao_nave_y = posy
-        self.vel_navex = vel_navex
-        self.vel_navey = vel_navey
+    def __init__(self,window,pos,imagem_redimensionada,vel_nave):
+        self.posicao_nave = pos
+        self.vel_nave = vel_nave
         self.window = window
         imagem = pygame.image.load('nave do joguinho.png')
         imagem_redimensionada = pygame.transform.scale(imagem, (100,100))
         self.imagem_redimensionada = imagem_redimensionada
 
     def desenha_nave(self):
-        self.window.blit(self.imagem_redimensionada, (self.posicao_nave_x, self.posicao_nave_y))
+        self.window.blit(self.imagem_redimensionada, (self.posicao_nave[0], 670))
 
-    def movimenta_nave(self):
+    def movimenta_nave(self, evento):
         
-        for evento in pygame.event.get():
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_d:
-                    self.vel_navex += 500
-                elif evento.key == pygame.K_a:
-                    self.vel_navex -= 500
-            if evento.type == pygame.KEYUP:
-                if evento.key == pygame.K_d:
-                    self.vel_navex -= 500
-                elif evento.key == pygame.K_a:
-                    self.vel_navex += 500
+        # for evento in pygame.event.get():
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_d:
+                self.vel_nave += 10
+            elif evento.key == pygame.K_a:
+                self.vel_nave -= 10
+
+        elif evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_d:
+                self.vel_nave -= 10
+            elif evento.key == pygame.K_a:
+                self.vel_nave += 10
+
+        self.posicao_nave[0] += self.vel_nave
 
 
 
