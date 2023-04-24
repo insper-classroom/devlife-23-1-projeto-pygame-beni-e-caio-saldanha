20885116f87ff5754b075f47f2ef9a71db3ba595
Benibergas <benibergas@gmail.com>
2023-04-24 08:21:04 -0300

    organizando o init

--- Classes_do_jogo.py
+++ Classes_do_jogo.py
@@ -5,20 +5,24 @@ from Classe_do_inimigo import Inimigo
 class Jogo:
     def __init__(self):
         pygame.init()
+        # TELA E WINDOW
         self.largura = 1520
         self.altura = 760
-        self.ultimo_updated = -1
-        self.delta_t = self.calcula_deltaT()
         self.window = pygame.display.set_mode((self.largura, self.altura))
         pygame.display.set_caption('Jogo do Caião e do Benizão')
+        # IMAGENS
         imagem_nave = pygame.image.load('nave do joguinho.png')
         imagem_nave_redimensionada = pygame.transform.scale(imagem_nave, (100,100))
         self.imagem_fundo = pygame.image.load('Imagens\Space Background.png')
         imagem_inimigo = pygame.image.load('Imagens\Klaed - Fighter - Base.png')
         imagem_inimigo_redimensionada = pygame.transform.scale(imagem_inimigo, (60,60))
+        # 
         self.nave = Nave(self.window, [380, 670], imagem_nave_redimensionada, 0, self.delta_t,1)
         self.nave2 = Nave(self.window, [1140, 670], imagem_nave_redimensionada, 0, self.delta_t,2)
         self.inimigo = Inimigo(self.window, [0,0], 8, imagem_inimigo_redimensionada)
+        # 
+        self.ultimo_updated = -1
+        self.delta_t = self.calcula_deltaT()
         
         #vaaaaaaaaaaaamos
 
