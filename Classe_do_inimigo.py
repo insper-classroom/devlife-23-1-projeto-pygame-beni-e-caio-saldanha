cd84575a95093486bc6b0bb8b9bfcd03f2bd3649
Benibergas <benibergas@gmail.com>
2023-04-24 09:54:19 -0300

    Organizando o init

--- Classe_do_inimigo.py
+++ Classe_do_inimigo.py
@@ -2,12 +2,17 @@ import pygame
 
 class Inimigo:
 
-    def __init__(self, window, pos, n_inimigos, imagem_redimensionada):
+    def __init__(self, window, pos, n_inimigos, imagem_redimensionada, largura_inimigo):
         self.window = window
         self.pos = pos
         self.n_inimigos = n_inimigos
         self.imagem = imagem_redimensionada
+        self.largura_inimigo = largura_inimigo
 
     def desenha_inimigo (self):
-        vida = self.imagem
-        self.window.blit(vida, self.pos)
+        inimigo = self.imagem
+        for i in range (4):
+            self.window.blit(inimigo, self.pos)
+            self.pos[0] += self.largura_inimigo
+            i += 1
+            
